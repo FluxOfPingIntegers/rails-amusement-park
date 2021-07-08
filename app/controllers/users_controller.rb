1cd3ba070261062f4bd06e9180d9e3dad4f268eb
FluxOfPingIntegers <Ryan.M.Schleck@gmail.com>
2021-07-07 22:10:23 -0400

    Done.

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,45 @@
+class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update]
+
+  def welcome
+  end
+
+  def index
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    user = User.create(user_params)
+    if user.save
+      session[:id] = user.id
+      redirect_to users_url(user)
+    else
+      flash[:errors] = user.errors.full_messages
+      redirect_to new_users_url
+    end
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+    def user_params
+      params.require(:user).permit(:name, :password, :nausea, :happiness, :tickets, :height, :admin)
+    end
+
+    def set_user
+      @user = User.find(session[:id])
+    end
+end
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,11 @@
 module UsersHelper
+
+  def logged_in?
+    !session[:id].nil?
+  end
+
+  def current_user
+    logged_in? ? User.find(session[:id]) : false
+  end
+
 end
